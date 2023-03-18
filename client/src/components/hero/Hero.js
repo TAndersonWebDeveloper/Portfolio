f45b9dfda8bf6edc055fa05d232a0aec757acea0
Tyler <anderson.tyler10@gmail.com>
2023-03-17 20:12:41 -0500

    add hero styles and animations

--- client/src/components/hero/Hero.js
+++ client/src/components/hero/Hero.js
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import { useState } from "react";
+import styled, { keyframes } from "styled-components";
 import tyler from "../../assets/tyler.JPG";
 const Container = styled.div`
   display: flex;
@@ -8,30 +9,50 @@ const Container = styled.div`
   justify-content: center;
   background: rgb(0, 8, 20);
   background: linear-gradient(
-    169deg,
+    90deg,
     rgba(0, 8, 20, 1) 0%,
-    rgba(0, 29, 61, 1) 68%
+    rgba(0, 29, 61, 1) 50%
   );
   color: white;
 `;
+
+const fadeIn = keyframes`
+
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }`;
 
 const TextWrapper = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
   margin-left: 15%;
+  animation: ${fadeIn} 1.5s ease-in-out;
+
+  padding-left: 20px;
+  height: 175px;
 `;
 
 const Title = styled.h1`
   font-size: 5rem;
+  padding-bottom: 20px;
+  padding-left: 20px;
+  border-left: 5px solid white;
+  animation: ${fadeIn} 1s ease-in-out;
 `;
 
 const Name = styled.span`
-  color: #ffd60a;
+  color: #02c39a;
 `;
 
 const Subtitle = styled.h2`
   font-size: 3rem;
+  animation: ${fadeIn} 1.5s ease-in-out;
+  border-left: 5px solid white;
+  padding-left: 20px;
 `;
 
 const ImageWrapper = styled.div`
@@ -39,7 +60,9 @@ const ImageWrapper = styled.div`
   display: flex;
   align-content: center;
   justify-content: center;
-  filter: drop-shadow(16px -32px 0px #000);
+  filter: drop-shadow(2px 2px 9px #4cc9f0);
+
+  animation: ${fadeIn} 1s ease-in-out;
 `;
 const Image = styled.img`
   width: 400px;
@@ -47,17 +70,32 @@ const Image = styled.img`
 `;
 
 const Hero = () => {
+  const [show, setShow] = useState(false);
+  const [titleShow, setTitleShow] = useState(false);
+  setTimeout(() => {
+    setTitleShow(true);
+  }, 1000);
+
+  setTimeout(() => {
+    setShow(true);
+  }, 2000);
+
   return (
     <Container>
       <TextWrapper>
-        <Title>
-          Hi, I'm<Name> Tyler</Name>.
-        </Title>
-        <Subtitle>I'm a full-stack web developer.</Subtitle>
+        {titleShow && (
+          <Title>
+            Hi, I'm<Name> Tyler</Name>.
+          </Title>
+        )}
+
+        {show && <Subtitle>I'm a full-stack web developer.</Subtitle>}
       </TextWrapper>
-      <ImageWrapper>
-        <Image src={tyler} alt="picture of tyler" />
-      </ImageWrapper>
+      {titleShow && (
+        <ImageWrapper>
+          <Image src={tyler} alt="picture of tyler" />
+        </ImageWrapper>
+      )}
     </Container>
   );
 };
--- client/src/components/ui/Navbar.js
+++ client/src/components/ui/Navbar.js
@@ -1,12 +1,24 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import { HiOutlineDocumentText } from "react-icons/hi";
+import { useState } from "react";
 const Container = styled.div`
   width: 100%;
-  height: 50px;
+  height: 60px;
   display: flex;
   justify-content: flex-end;
   position: fixed;
+`;
+
+const fromTop = keyframes`
+from {
+  opacity: 0;
+  transform: translateY(-100%);
+}
+to {
+  opacity: 1;
+  transform: translateY(0);
+}
 `;
 
 const LinksList = styled.ul`
@@ -17,6 +29,7 @@ const LinksList = styled.ul`
   height: 100%;
   width: 10%;
   margin-right: 2rem;
+  animation: ${fromTop} 1.5s ease-in-out;
 `;
 
 const LinkItem = styled.li`
@@ -26,34 +39,42 @@ const LinkItem = styled.li`
 const StyledLink = styled.a``;
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  setTimeout(() => {
+    setShow(true);
+  }, 2000);
+
   return (
     <Container>
-      <LinksList>
-        <LinkItem>
-          <StyledLink
-            href="https://www.linkedin.com/in/tyler-anderson-74766722b/"
-            target="_blank"
-          >
-            <AiFillLinkedin className="linkedin" />
-          </StyledLink>
-        </LinkItem>
-        <LinkItem>
-          <StyledLink
-            href="https://github.com/TAndersonWebDeveloper"
-            target="_blank"
-          >
-            <AiFillGithub className="github" />
-          </StyledLink>
-        </LinkItem>
-        <LinkItem>
-          <StyledLink
-            href="https://github.com/TAndersonWebDeveloper"
-            target="_blank"
-          >
-            <HiOutlineDocumentText className="resume" />
-          </StyledLink>
-        </LinkItem>
-      </LinksList>
+      {show && (
+        <LinksList>
+          <LinkItem>
+            <StyledLink
+              href="https://www.linkedin.com/in/tyler-anderson-74766722b/"
+              target="_blank"
+            >
+              <AiFillLinkedin className="linkedin" />
+            </StyledLink>
+          </LinkItem>
+          <LinkItem>
+            <StyledLink
+              href="https://github.com/TAndersonWebDeveloper"
+              target="_blank"
+            >
+              <AiFillGithub className="github" />
+            </StyledLink>
+          </LinkItem>
+          <LinkItem>
+            <StyledLink
+              href="https://github.com/TAndersonWebDeveloper"
+              target="_blank"
+            >
+              <HiOutlineDocumentText className="resume" />
+            </StyledLink>
+          </LinkItem>
+        </LinksList>
+      )}
     </Container>
   );
 };
